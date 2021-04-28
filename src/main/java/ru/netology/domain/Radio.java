27745b9efbdca0036a2c0d2d9b2f19689a708942
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-28 04:25:07 +0400

    Create Radio.java

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class Radio {
+    String name;
+    int maxVolume;  //Максимальный звук
+    int minVolume; //Минимальный звук
+    int currentVolume; //Текущий звук
+    int maxRadioChannel; //Максимальный радио канал
+    int minRadioChannel; //Минимальный радио канал
+    int currentRadioChannel; //Текущий радио канал
+    boolean on; // Включен\Выключен
+}
