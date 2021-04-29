6e4be2deddf2869c0624d54c11f3d6f2e4cb7c16
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-29 04:55:02 +0400

    Update Radio.java

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -2,11 +2,118 @@ package ru.netology.domain;
 
 public class Radio {
     String name;
-    int maxVolume = 10;  //Максимальный звук
-    int minVolume = 0; //Минимальный звук
-    int currentVolume = 5; //Текущий звук
-    int maxRadioChannel = 9; //Максимальный радио канал
-    int minRadioChannel = 1; //Минимальный радио канал
-    int currentRadioChannel = 5; //Текущий радио канал
-    boolean on; // Включен\Выключен
+
+    int maxRadioChannel; //Максимальный радиоканал
+    int minRadioChannel; //Минимальный радиоканал
+    int currentRadioChannel; //Текущий радиоканал
+
+    int maxVolume;  //Максимальный звук
+    int minVolume; //Минимальный звук
+    int currentVolume; //Текущий звук
+
+    public int getMaxRadioChannel() {
+        return maxRadioChannel;
+    }
+
+    public void setMaxRadioChannel(int maxRadioChannel) {
+        this.maxRadioChannel = maxRadioChannel;
+    }
+
+    public int getMinRadioChannel() {
+        return minRadioChannel;
+    }
+
+    public void setMinRadioChannel(int minRadioChannel) {
+        this.minRadioChannel = minRadioChannel;
+    }
+
+    public int getCurrentRadioChannel() {
+        return currentRadioChannel;
+    }
+
+    public void setCurrentRadioChannel(int currentRadioChannel) {
+        if (currentRadioChannel > maxRadioChannel) {
+            return;
+        }
+        if (currentRadioChannel < minRadioChannel) {
+            return;
+        }
+        this.currentRadioChannel = currentRadioChannel;
+    }
+
+    public void nextCurrentRadioChannel() {
+        int currentRadioChannel = this.currentRadioChannel;
+        if (currentRadioChannel >= maxRadioChannel) {
+            this.currentRadioChannel = minRadioChannel;
+        } else {
+            this.currentRadioChannel = currentRadioChannel + 1;
+        }
+    }
+
+    public void prevCurrentRadioChannel() {
+        int currentRadioChannel = this.currentRadioChannel;
+        if (currentRadioChannel <= minRadioChannel) {
+            this.currentRadioChannel = maxRadioChannel;
+        } else {
+            this.currentRadioChannel = currentRadioChannel - 1;
+        }
+    }
+
+    public void remoteCurrentRadioChannel(int currentRadioChannel) {
+        if (currentRadioChannel > maxRadioChannel) {
+            this.currentRadioChannel = maxRadioChannel;
+        } else if (currentRadioChannel < minRadioChannel) {
+            this.currentRadioChannel = minRadioChannel;
+        } else {
+            this.currentRadioChannel = currentRadioChannel;
+        }
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void nextCurrentVolume() {
+        int currentVolume = this.currentVolume;
+        if (currentVolume >= maxVolume) {
+            this.currentVolume = maxVolume;
+        } else {
+            this.currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void prevCurrentVolume() {
+        int currentVolume = this.currentVolume;
+        if (currentVolume <= minVolume) {
+            this.currentVolume = minVolume;
+        } else {
+            this.currentVolume = currentVolume - 1;
+        }
+    }
 }
